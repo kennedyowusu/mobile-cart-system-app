c147fbbb5fe34494fced58aef30a7306f05b8061
Kennedy Owusu <kennediowusu@gmail.com>
2023-06-18 10:38:03 +0000

    remove items from database

--- script.js
+++ script.js
@@ -2,8 +2,16 @@ const appSettings = {
  databaseURL: 'https://playground-60395-default-rtdb.firebaseio.com/',
 }
 
-import { initializeApp } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js';
-import { getDatabase, ref, push, onValue } from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js';
+import {
+ initializeApp
+} from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js';
+import {
+ getDatabase,
+ ref,
+ push,
+ onValue,
+ remove
+} from 'https://www.gstatic.com/firebasejs/9.0.2/firebase-database.js';
 
 const app = initializeApp(appSettings);
 const database = getDatabase(app);
@@ -14,13 +22,11 @@ const inputFieldElement = document.getElementById('input-field');
 const listElement = document.getElementById('shopping-list');
 
 
-
 inputBtn.addEventListener('click', () => {
  let inputValue = inputFieldElement.value;
  push(shoppingListInDB, inputValue)
 
  clearInputFieldElement();
-
 });
 
 onValue(shoppingListInDB, (snapshot) => {
@@ -28,7 +34,7 @@ onValue(shoppingListInDB, (snapshot) => {
  snapshot.forEach((childSnapshot) => {
   let childKey = childSnapshot.key;
   let childData = childSnapshot.val();
-  appendItemToShoppingListElement(childData);
+  appendItemToShoppingListElement(childData, childKey);
  });
 });
 
@@ -40,6 +46,17 @@ function clearInputFieldElement() {
  inputFieldElement.value = '';
 }
 
-function appendItemToShoppingListElement(item) {
- listElement.innerHTML += `<li>${item}</li>`;
+function appendItemToShoppingListElement(item, key) {
+ let liElement = document.createElement('li');
+ liElement.innerText = item;
+ listElement.appendChild(liElement);
+
+ liElement.addEventListener('click', () => {
+  removeItemFromShoppingListElement(key);
+ });
+}
+
+function removeItemFromShoppingListElement(key) {
+ let exactLocationOfItemInDB = ref(database, `shopping-list/${key}`);
+ remove(exactLocationOfItemInDB);
 }
